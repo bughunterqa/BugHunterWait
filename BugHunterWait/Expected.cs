8a4ede62401cd2d80b17f4a4d82208cade4726de
Vlados Rybalka <ribalka.vladik@gmail.com>
2024-01-09 13:17:27 +0200

    Add project files.

---
+++ BugHunterWait/Expected.cs
@@ -0,0 +1,50 @@
+﻿using OpenQA.Selenium;
+
+namespace BugHunterWait
+{
+    public class Expected
+    {
+        private static Func<IWebDriver, IWebElement> ElementIsVisible(IWebElement element)
+        {
+            return delegate (IWebDriver driver)
+            {
+
+                try
+                {
+                    if (element != null && element.Displayed)
+                    {
+                        return element;
+                    }
+
+                    return null;
+                }
+                catch (StaleElementReferenceException)
+                {
+                    return null;
+                }
+            };
+        }
+
+        private static Func<IWebDriver, IWebElement> ElementIsInvisible(IWebElement element)
+        {
+            return (driver) =>
+            {
+                try
+                {
+                    if (element != null && !element.Displayed)
+                    {
+                        return element;
+                    }
+                    else
+                    {
+                        return null;
+                    }
+                }
+                catch (StaleElementReferenceException)
+                {
+                    return null;
+                }
+            };
+        }
+    }
+}
